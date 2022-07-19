faac4e781717c21375147c4b56ccf0d99055ba2e
Billy Arredondo <billy.arredondo@pucp.pe>
2022-07-19 15:52:02 -0500

    Learn Custom Hook for Form
    FormWithCustomHook is calling the useForm custom hook

---
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useForm } from "../hooks/useForm";
+import { Message } from "./Message";
+
+export const FormWithCustomHook = () => {
+
+    const { username, email, password, onInputChange } = useForm({
+        username: "",
+        email: "",
+        password: ""
+    });
+
+    return (
+        <>
+            <h1>Form with Custom Hook</h1>
+            <hr />
+
+            <input
+                type="text"
+                className="form-control"
+                placeholder="username"
+                name="username"
+                value={username}
+                onChange={onInputChange}
+            />
+
+            <input
+                type="email"
+                className="form-control mt-2"
+                placeholder="email@domain.com"
+                name="email"
+                value={email}
+                onChange={onInputChange}
+            />
+
+            <input
+                type="password"
+                className="form-control mt-2"
+                placeholder="password"
+                name="password"
+                value={password}
+                onChange={onInputChange}
+            />
+
+            {
+                (username === 'gubiarpa') && <Message />
+            }
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { SimpleForm } from "./02-useEffect/SimpleForm";
+import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook.jsx";
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <SimpleForm />
-  // </React.StrictMode>
+    <FormWithCustomHook />
 )
