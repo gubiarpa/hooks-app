0a911fdf7cbf2fe6885b98f7d94384d6bd52db6f
Billy Arredondo <billy.arredondo@pucp.pe>
2022-07-19 01:26:13 -0500

    Learn useEffect
    We're trying with different dependencies of the component.
    For learning purpose, we've removed the React.StrictMode in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
+import { SimpleForm } from "./02-useEffect/SimpleForm";
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CounterWithCustomHook />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <SimpleForm />
+  // </React.StrictMode>
 )
