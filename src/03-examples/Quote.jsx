adaa9317664cdc70d3bf271f6f14259fa15be001
Billy Arredondo <billy.arredondo@pucp.pe>
2022-07-23 01:52:56 -0500

    Learning useLayoutEffect

--- src/03-examples/Quote.jsx
+++ src/03-examples/Quote.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export const Quote = ({ quote, author }) => {
+
+    const pRef = useRef();
+    const [boxSize, setBoxSize] = useState({ width: 0, height: 0 });
+
+    useEffect(() => {
+        const { height, width } = (pRef.current.getBoundingClientRect());
+        setBoxSize({ height, width });
+    }, [quote])
+
     return (
-        <blockquote className="text-end">
-            <p className="mb-1">{quote}</p>
-            <footer className="mt-2 blockquote-footer">
-                {author}
-            </footer>
-        </blockquote>
+        <>
+            <blockquote
+                className="text-end"
+                style={{ display: 'flex' }}
+            >
+                <p ref={pRef} className="mb-1">{quote}</p>
+                <footer className="mt-2 blockquote-footer">
+                    {author}
+                </footer>
+            </blockquote>
+
+            <code>{JSON.stringify(boxSize)}</code>
+        </>
     )
 }
---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useCounter, useFetch } from "../hooks";
+import { LoadingQuote, Quote } from "../03-examples/index";
+
+export const Layout = () => {
+
+    const {
+        counter,
+        incrementOne
+    } = useCounter(1);
+
+    const {
+        data,
+        isLoading,
+        hasError
+    } = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+
+    const { author, quote } = !!data && data[0];
+
+    return (
+        <>
+            <h1>Breaking Bad Quotes</h1>
+            <hr />
+
+            <button
+                className="btn btn-outline-primary"
+                onClick={() => { incrementOne(1) }}
+                disabled={isLoading}
+            >
+                Next quote
+            </button>
+            <hr />
+
+            {
+                isLoading ?
+                    <LoadingQuote /> :
+                    <Quote author={author} quote={quote} />
+            }
+
+        </>
+    );
+}
