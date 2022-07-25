3c698c0b36c2fed08edfedc8e38876baf0df6432
Billy Arredondo <billy.arredondo@pucp.pe>
2022-07-24 20:54:03 -0500

    Learning useMemo

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Memorize } from "./06-memos/Memorize";
+import { MemoHook } from "./06-memos/MemoHook";
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Memorize />
+    <MemoHook />
 )
