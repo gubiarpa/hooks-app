71ace3f1f6e91484b09f7d09ca5e69efa1ffeb16
Billy Arredondo <billy.arredondo@pucp.pe>
2022-08-01 23:10:23 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './08-useReducer/intro-reducer'
-import { TodoApp } from "./08-useReducer/TodoApp";
+import { MainApp } from "./09-useContext/MainApp";
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <TodoApp />
+    <MainApp />
 )
