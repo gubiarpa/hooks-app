882c31fd7d0226a8de3f23af1bc4d7906b172183
Billy Arredondo <billy.arredondo@pucp.pe>
2022-08-01 23:28:27 -0500

    Adding Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import './08-useReducer/intro-reducer'
 import { MainApp } from "./09-useContext/MainApp";
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <MainApp />
+    <BrowserRouter>
+        <MainApp />
+    </BrowserRouter>
 )
