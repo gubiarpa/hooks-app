120bd50a502063cb45e1cc87208c09a63e8c3e32
Billy Arredondo <billy.arredondo@pucp.pe>
2022-08-03 16:47:14 -0500

    Testing MultipleCustomHooks - Part 1
    tests:
    - should show component by default
    - should show a quote

--- tests/03-examples/MultipleCustomHooks.test.jsx
+++ tests/03-examples/MultipleCustomHooks.test.jsx
@@ -1,9 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import { MultipleCustomHooks } from "../../src/03-examples";
+import { useFetch } from "../../src/hooks/useFetch";
+
+jest.mock("../../src/hooks/useFetch");
 
 describe('testing on <MultipleCustomHooks />', () => {
 
     test('should show component by default', () => {
+
+        useFetch.mockReturnValue({
+            data: null,
+            isLoading: true,
+            hasError: null
+        });
 
         render(<MultipleCustomHooks />);
 
@@ -12,8 +21,30 @@ describe('testing on <MultipleCustomHooks />', () => {
 
         const nextButton = screen.getByRole('button');
 
-        expect(nextButton.disabled).toBe(true);
+        expect(nextButton.disabled).toBeTruthy();
+        
+    });
+    
+    test('should show a quote', () => {
+        
+        useFetch.mockReturnValue({
+            data: [{
+                author: "Billy",
+                quote: "Hello world"
+            }],
+            isLoading: false,
+            hasError: null
+        });
+        
+        render(<MultipleCustomHooks />);
+        
+        expect(screen.getByText("Hello world")).toBeTruthy();
+        expect(screen.getByText("Billy")).toBeTruthy();
+        
+        
+        const nextButton = screen.getByRole('button');
+        expect(nextButton.disabled).toBeFalsy();
 
-    })
+    });
 
 });
