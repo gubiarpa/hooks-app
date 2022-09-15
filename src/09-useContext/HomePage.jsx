1b59aa6efdfec95f82a5075e6c5d84fbadf92d9c
Billy Arredondo <billy.arredondo@pucp.pe>
2022-09-14 19:24:27 -0500

    Using useCounter on HomePage

--- src/09-useContext/HomePage.jsx
+++ src/09-useContext/HomePage.jsx
@@ -1,14 +1,23 @@
 import React, { useContext } from "react";
+
 import { UserContext } from "./context/UserContext";
+import { useCounter } from "../hooks/useCounter";
 
 export const HomePage = () => {
 
     const { user } = useContext(UserContext);
+    const { counter, incrementOne } = useCounter(29);
 
     return (
         <>
-            <h1>HomePage</h1>
+            <h1>HomePage on {counter}</h1>
             <hr />
+            <button
+                className="btn btn-outline-primary"
+                onClick={() => { incrementOne() }}
+            >
+                Increase
+            </button>
 
             <pre>
                 {user && JSON.stringify(user, null, 3)}
